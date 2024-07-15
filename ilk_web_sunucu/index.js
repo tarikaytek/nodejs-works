a8ac08d004d1ea7b014e7d5f0d2f9a8c0e34a682
tarikaytek <aytek.tarik1@gmail.com>
2024-07-15 03:36:50 +0300

    add odev5

---
+++ ilk_web_sunucu/index.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    console.log(url);
+
+    if (url === "/") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>index sayfasına hoşgeldin</h2>");
+      } else if (url === "/hakkimda") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>hakkimda sayfasina hosgeldin</h2>");
+      } else if (url === "/iletisim") {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.write("<h2>iletisim sayfasina hosgeldin</h2>");
+      } else {
+        res.writeHead(404, { "Content-Type": "text/html" });
+        res.write("<h1>404 SAYFA BULUNAMADI</h1>");
+      }
+
+    res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+    console.log(`Sunucu port ${port}'da baslatildi`);
+});
+
